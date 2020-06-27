6c946958164efffe8c7422e9ba774d63a916c34a
andreiersek <67473278+andreiersek@users.noreply.github.com>
2020-06-27 17:16:04 +0300

    Add files via upload

---
+++ embeded.py
@@ -0,0 +1,30 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+rlinpin=8
+hallpin=12
+command= False
+GPIO.setup(rlinpin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(hallpin, GPIO.IN)
+
+while True:
+    if (command == 1):
+        GPIO.output(rlinpin, GPIO.HIGH)
+        print("deblocat")
+        sleep(5)
+        GPIO.output(rlinpin, GPIO.LOW)
+        print("blocat")
+    else:
+        GPIO.output(rlinpin, GPIO.LOW)
+        print("blocat")
+    
+    if(GPIO.input(hallpin)== True):
+        print("Usa deschisa")
+        sleep(3)
+        
+    else:
+        print("usa inschisa")
+        sleep(3)
+    
+    command = not command
